ccb80275ef91cb3186e5aeb6e0ce5be0176b4f54
eab70 <eabacus@msn.com>
2015-07-23 10:38:12 -0400

    no message

---
+++ ChangeReturn/Labs Numbers4/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labs_Numbers4
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine(
+                "The items available for purchase in this vending machine are: popcorn, $1.25; peanuts, $2.00; Hershey kisses, $1.50; ice cream sandwich, $0.75; apple juice, $1.00.");
+
+            Console.WriteLine("What is the price of the item you wish to purchase: ");
+            decimal userInput = decimal.Parse(Console.ReadLine());
+
+            Console.WriteLine("The amount of money you inserted into the vending machine is: ");
+            decimal userInput2 = decimal.Parse(Console.ReadLine());
+
+            string[] change = ChangeCalc(userInput, userInput2);
+
+            Console.WriteLine("Your change is {0} quarters, {1} dimes, {2} nickles and {3} pennies.", change[0],
+                change[1], change[2], change[3]);
+            Console.ReadLine();
+        }
+
+        public static string[] ChangeCalc(decimal a, decimal b)
+        {
+            string[] coins = new string[4];
+
+            if (a > b)
+            {
+                Console.WriteLine("That is not enough money for the item.");
+            }
+            else
+            {
+                decimal c = b - a;
+                decimal quarters;
+                decimal dimes;
+                decimal nickles;
+                decimal pennies;
+
+                if (c >= 0.25M)
+                {
+                    quarters = (c/25)*100;
+                    decimal numOfQuarters = Math.Floor(quarters);
+                    c = c - numOfQuarters*25/100;
+                    string quarter2 = numOfQuarters.ToString("0");
+                    coins[0] = quarter2;
+                }
+
+                if (c >= 0.10M)
+                {
+                    dimes = (c/10)*100;
+                    decimal numOfDimes = Math.Floor(dimes);
+                    c = c - numOfDimes*10/100;
+                    string dimes2 = dimes.ToString("0");                    
+                    coins[1] = dimes2;
+                }
+
+                if (c >= 0.05M)
+                {
+                    nickles = (c/5)*100;
+                    decimal numOfNickles = Math.Floor(nickles);
+                    c = c - numOfNickles*5/100;
+                    string nickles2 = nickles.ToString("0");
+                    coins[2] = nickles2;
+                }
+
+                if (c < 0.05M && c > 0)
+                {
+                    pennies = (c/1)*100;
+                    decimal numOfPennies = Math.Floor(pennies);
+                    string c2 = numOfPennies.ToString("0");
+                    coins[3] = c2;
+                }
+            }
+            return coins;
+        }
+    }
+}
+
+
+
+
+
+//Change Return Program
+//Difficulty: 2
+
+//Create a program where it shows the user a list of items to buy and their price. Then ask the user to pick an item and enter in the 
+//amount of money they would have inserted into the “vending machine”. Have the program calculate the change and return it to the user in 
+//the form of quarters, dimes, nickels and pennies. For example, if the user chooses an item that costs $1.25 and they say they give 
+//it $2.07 the program would print out 3 quarters, 1 nickel and 2 pennies as change for the user.
+
+//Tips
+//-------------------------------
+//First find out the difference between the item they bought and how much they are paying. Then look at using integer division to 
+//first calculate how many coins of each type to return. Then use the modulus (% operator in most languages) to determine the remainder 
+//of the change and repeat the process for each denomination. Be sure to check that the money they have inserted will first cover the price
+//of the item! For example 89 cents / 25 is 3. This means 3 quarters with 14 cents returned.
---
+++ CommonDenom/Labs Nums 3CommonDenom/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Labs_Nums_3CommonDenom
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.Write("Please enter the first of two fractions: ");
+            string userInput = Console.ReadLine();
+            Console.Write("Please enter the second fraction: ");
+            string userInput2 = Console.ReadLine();
+            
+            int common = LargestCommonDenom(userInput, userInput2);
+            int common2 = LeastCommonDenom(userInput, userInput2);
+            Console.WriteLine("The least common denominator is: {0}, and the largest common denominator: {1}", common2,
+                common);
+
+            Console.ReadLine();
+        }
+        
+        public static int LeastCommonDenom(string a, string b)
+        {
+            string[] a2 = a.Split('/');
+            string a3 = a2[1];
+            int a4 = int.Parse(a3);
+
+            string[] b2 = b.Split('/');
+            string b3 = b2[1];
+            int b4 = int.Parse(b3);
+            int a4b4 = a4*b4;
+            
+
+            for (int i = 2; i <a4b4; i++)
+            {
+                if (a4 %b4 == 0)
+                {
+                    return a4;
+                }
+                else if (b4%a4 == 0)
+                {
+                    return b4;
+                }
+                else if (a4%i == 0 && b4%i == 0)
+                {
+                    return i;
+                }
+                i++;
+                
+            }return a4b4;
+        }
+
+
+        public static int LargestCommonDenom(string a, string b)
+        {
+            string[] a2 = a.Split('/');
+            string a3 = a2[1];
+            int a4 = int.Parse(a3);
+
+            string[] b2 = b.Split('/');
+            string b3 = b2[1];
+            int b4 = int.Parse(b3);
+
+            int ab = a4*b4;
+            return ab;
+        }
+    }
+}
+
+//However, fractions not simplified.
+
+//LCD/GCD Least/Greatest Common Denominator
+//Difficulty: 3
+//Create a program that asks the user to enter two fractions. Have the program find the least 
+//common or the greatest common denominator between those two fractions and print it out.
+//Tips
+//Make sure to isolate the denominator first and find a multiple that goes into each of the 
+//denominators. You can start by finding the least common multiple (the smallest integer value 
+//that fits into both denominators). The greatest common denominator is the largest integer value 
+//which fits into both denominators.
+
+//Added Difficulty
+//Try working on more than two terms.
